98376e1dab6af29872f4badcb2b84c64821d872e
kishore <skill2automate@gmail.com>
2026-08-06 08:16:42 +0530

    fixed failure

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -12,7 +12,7 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();
 
 
   // Expects page to have a heading with the name of Installation.
